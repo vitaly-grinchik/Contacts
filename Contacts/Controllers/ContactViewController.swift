fc19893fb4d76f5ab447f00967779b1df8c810a6
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-01-11 15:23:04 +0300

    Unnecessary empty ines delete

--- Contacts/Controllers/ContactViewController.swift
+++ Contacts/Controllers/ContactViewController.swift
@@ -20,16 +20,4 @@ class ContactViewController: UIViewController {
         phoneLabel.text = contact.phone
         emailLabel.text = contact.email
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
