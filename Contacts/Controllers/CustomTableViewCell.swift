563c573fe188aa249f175a500a226517c026f03c
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-01-12 22:11:06 +0300

    DetailedListViewController editing

---
+++ Contacts/Controllers/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  Contacts
+//
+//  Created by Виталий Гринчик on 11.01.23.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Contacts/Controllers/DetailedListViewController.swift
+++ Contacts/Controllers/DetailedListViewController.swift
@@ -17,19 +17,27 @@ class DetailedListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 2
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "richCell", for: indexPath)
-        let person = contacts[indexPath.row]
-        
+        let person = contacts[indexPath.section]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailedCell", for: indexPath)
+    
         var cellContent = cell.defaultContentConfiguration()
-        cellContent.text = person.fullName
-        cell.contentConfiguration = cellContent
-        
+        if indexPath.row == 0 {
+            cellContent.text = person.phone
+            cellContent.image = UIImage(systemName: "phone")
+            cell.contentConfiguration = cellContent
+        } else {
+            cellContent.text = person.email
+            cellContent.image = UIImage(systemName: "envelope")
+            cell.contentConfiguration = cellContent
+        }
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        contacts[section].fullName
+    }
 }
